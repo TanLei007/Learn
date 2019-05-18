f1c50917094872022808580235249587aec5a689
tanlei <1015006581@qq.com>
2019-05-18 16:30:25 +0800

    增加分支

--- springboot-kafka-api/src/main/java/com/gl/springbootkafkaapi/SpringbootKafkaApiApplication.java
+++ springboot-kafka-api/src/main/java/com/gl/springbootkafkaapi/SpringbootKafkaApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootKafkaApiApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringbootKafkaApiApplication.class, args);
     }
 
