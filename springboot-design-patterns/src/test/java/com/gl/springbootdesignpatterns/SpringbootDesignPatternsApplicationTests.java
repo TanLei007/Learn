d599bf4d6933fb1e7fa3329ea825c701dac9288f
tanlei <1015006581@qq.com>
2019-05-18 16:39:31 +0800

    增加分支

---
+++ springboot-design-patterns/src/test/java/com/gl/springbootdesignpatterns/SpringbootDesignPatternsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gl.springbootdesignpatterns;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootDesignPatternsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
