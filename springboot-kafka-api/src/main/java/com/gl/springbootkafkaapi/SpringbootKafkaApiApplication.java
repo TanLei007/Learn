6835a18b6ab76fb1129430c69a819177d6ca017d
tanlei <1015006581@qq.com>
2019-05-18 21:08:15 +0800

    增加装饰模式

--- springboot-kafka-api/src/main/java/com/gl/springbootkafkaapi/SpringbootKafkaApiApplication.java
+++ springboot-kafka-api/src/main/java/com/gl/springbootkafkaapi/SpringbootKafkaApiApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootKafkaApiApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(SpringbootKafkaApiApplication.class, args);
     }
 
